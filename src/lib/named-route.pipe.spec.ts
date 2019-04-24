eec85d49d8661f8a1b1d384b76cbead655e5d364
SaeedPooyanfar <saeedp47@gmail.com>
2019-04-24 18:49:18 +0430

    namedRoutesService instance for NamedRoutePipe class

--- src/lib/named-route.pipe.spec.ts
+++ src/lib/named-route.pipe.spec.ts
@@ -1,11 +1,11 @@
 import { NamedRoutePipe } from './named-route.pipe';
 import { TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
+import { NamedRoutesService } from './named-routes.service';
 
 describe('NamedRoutePipe', () => {
   it('create an instance', () => {
-    const router = TestBed.get(Router);
-    const pipe = new NamedRoutePipe(router);
+    const namedRoutesService = TestBed.get(NamedRoutesService);
+    const pipe = new NamedRoutePipe(namedRoutesService);
     expect(pipe).toBeTruthy();
   });
 });
